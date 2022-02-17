294228faffa7fcc3423f037d96ac44f35a42856f
Lonerangerix <1137198072@qq.com>
2022-02-17 02:00:52 +0100

    Half-finished poster and tags implementation

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -10,6 +10,7 @@ import { parseData, parseCsv } from "../../utils/fetchData";
 
 import { SearchBox } from '../searchbox'
 import { ContainerBox } from "../containerbox";
+import { tags } from "../tags/tags";
 
 
 // Provide an onChange function on a Dropdown component to process the updated data.
@@ -55,7 +56,6 @@ const Checkbox = (props) => {
     </div>
   )
 }
-
 export const Main = (props) => {
 
   //setup for the scatter plot 
@@ -88,15 +88,71 @@ export const Main = (props) => {
     })
   }, []);
 
+
+
   const onSearchBoxSubmit = (event) => {
     event.preventDefault()
     console.log(event.target[0].value)
   }
 
+  const [tagsCheckedState, setTagsCheckedState] = useState(
+    new Array(tags.length).fill(false)
+  );
+  const handleTagsOnChange = (position) => {
+    const updatedCheckedState = tagsCheckedState.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setTagsCheckedState(updatedCheckedState);
+
+    /*updatedCheckedState.reduce((currentState, index)=>{
+      if(currentState===true)
+      {
+        console.log(tags[index].tagName);
+      }
+    });*/
+    if(updatedCheckedState[position]===true)
+    {
+      //console.log(tags[position].tagName);
+      newTagSelected(tags[position].tagName);
+      console.log(tagsSelected);
+        parseData((result) => {
+          // onsole.log(result.data);
+        console.log(processData(result.data));
+        setRawData(processData(result.data));//how to refresh?
+        
+        })
+     
+      
+    }
+    //console.log();    
+
+  };
+
   return (
     <div className={`${props.className ? props.className : ''} col-span-full main-grid pr-4 py-4`}>
       <SearchBox onSubmit={onSearchBoxSubmit} className="col-span-4" />
-      <ContainerBox title="Tags" className="row-start-2 col-span-3" />
+      <ContainerBox title="Tags" style={{height:500}} className="row-start-2 col-start-1 col-span-3 ">
+        <div className="row-start-1 col-span-full" style={{overflowY: 'scroll',height:300}}>
+        <ul className="tags-list">
+        {tags.map(({ tagName }, index) => {
+
+            return (
+              <li key={index}>
+                <div className="tags-list-item text-white">
+                  <Checkbox name={tagName} label={tagName} onChange={()=>handleTagsOnChange(index)} />
+                </div>
+              </li>
+            );
+
+          
+        })}
+        </ul>
+        </div>
+
+      </ContainerBox>
+
+
       <ContainerBox title="Filters" className="row-start-2 col-start-4 col-span-full filter-grid p-5">
         <Dropdown
           label="X - Axis"
@@ -153,18 +209,56 @@ export const Main = (props) => {
         </div>
       </ContainerBox>
       <div className="bg-gray-100 row-start-3 col-span-5">
-        {rawData && <ScatterPlot settings={settings} rawData={rawData} />}
+        {rawData && <ScatterPlot id="scatterPlot" settings={settings} rawData={rawData} />}
       </div>
-      <ContainerBox title="Info" className="row-start-3 col-start-6 col-span-3" />
+      <ContainerBox title="Info" className="row-start-3 col-start-6 col-span-3" >
+      <div className="col-start-6 row-start-3 row-span-2 text-white">
+        <p id="animeName" className="text-m justify-self-center text-center font-bold">Name</p>        
+        <img id="animePoster" className="align-self-center justify-self-center" src="https://cdn.anime-planet.com/anime/primary/fairy-tail-1.jpg" />
+      </div>
+      </ContainerBox>
       <ContainerBox title="Range" className="row-start-4 col-span-5" />
       <ContainerBox title="Related" className="row-start-4 col-start-6 col-span-full" />
     </div>
+    
   )
 }
-
+const tagsSelected=[]
 const processData = (data) => {
   // Here maybe add other filters 
   // call this function whenever add new filter
   let returnData = data.filter(row => row[8] > 4);
+  returnData=returnData.filter(function (row){
+    if(row[7]!==null)
+    {return filterWithTags(row[7])}
+    else
+    {return false}
+  })
   return returnData;
+}
+const filterWithTags=(tagString)=>{
+  
+  return tagsSelected.every(function (tag){
+    return tagString.includes(tag)
+  });
+}
+const newTagSelected=(tag)=>{
+  tagsSelected.push(tag);
+}
+//change the name and the poster
+export const refreshInfo = (name) => {
+
+  const animeName = name;
+  document.getElementById("animeName").textContent = animeName;
+  //var posterUrl=animeName.replace('Conan (.*?):','');
+  var posterUrl = animeName.replace('\'', '').replace(/[^\u2018-\u2019\u4e00-\u9fa5a-zA-Z0-9]/g, '-').replaceAll("---", '-').replaceAll("--", '-').toLowerCase();
+  console.log(posterUrl[posterUrl.length - 1]);
+  if (posterUrl[posterUrl.length - 1] == '-') {
+    posterUrl = posterUrl.slice(0, posterUrl.length - 1);
+  }
+
+  document.getElementById("animePoster").src = "https://cdn.anime-planet.com/anime/primary/" + posterUrl + "-1.jpg";
+  console.log(animeName);
+  console.log(posterUrl);
+
 }
