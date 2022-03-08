6e267ea4598dc564a1b137c8317edab8b72f350a
xinmiao <>
2022-03-08 10:23:44 +0100

    fix - circle display bug

--- src/pages/Demo/demo.js
+++ src/pages/Demo/demo.js
@@ -4,11 +4,11 @@ import './demo.css';
 export const Demo = (props) => {
   return (
     <div className={`${props.className ? props.className : ''} col-span-full font-ssp flex flex-col justify-around px-8` }>
-      <div className="circle1 absolute bg-gradient-to-b from-circle1-top to-circle1-bottom"></div>
-      <div className="circle2 absolute bg-gradient-to-b from-circle2-top to-circle2-bottom"></div>
-      <div className="circle3 absolute bg-gradient-to-b from-circle3-top to-circle3-bottom"></div>
-      <div className="circle4 absolute bg-gradient-to-b from-circle4-top to-circle4-bottom"></div>
-      <div className="circle5 absolute bg-gradient-to-b from-circle5-top to-circle5-bottom"></div>
+      <div className="circle01 absolute bg-gradient-to-b from-circle1-top to-circle1-bottom"></div>
+      <div className="circle02 absolute bg-gradient-to-b from-circle2-top to-circle2-bottom"></div>
+      <div className="circle03 absolute bg-gradient-to-b from-circle3-top to-circle3-bottom"></div>
+      <div className="circle04 absolute bg-gradient-to-b from-circle4-top to-circle4-bottom"></div>
+      <div className="circle05 absolute bg-gradient-to-b from-circle5-top to-circle5-bottom"></div>
 
     </div>
   )
